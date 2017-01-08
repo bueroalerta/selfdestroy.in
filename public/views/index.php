8def535164b9f0aaaec8005e6dafdf7517f82874
Xavian Ang <xavianang@openmindsresources.com>
2017-01-08 22:18:19 +0800

    update title

--- public/views/index.php
+++ public/views/index.php
@@ -5,7 +5,7 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>SelfDestroy.in Messages</title>
+  <title>selfdestroy.in your encrypted messages easily</title>
 
   <!-- CSS -->
   <link rel="stylesheet" href="bower_resources/bootstrap/dist/css/bootstrap.min.css"/>
